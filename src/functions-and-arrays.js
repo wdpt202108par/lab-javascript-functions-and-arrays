c47e281460cadbded4b7faa529ce9bf0fabd2b37
Claire Pottier <claire_pottier@hotmail.fr>
2021-09-04 14:37:02 +0200

    1ère itération + essai 2ème itération

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,12 +1,23 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  return Math.max(num1, num2)
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  let longestWord = [];
+  for (i = 0; i < words.length; i++) {
+          if (words[i].length > 0){
+            longestWord= arr[i].length;
+          }
+      }
+      return longestWord;
+    }
+  
 
 
 
